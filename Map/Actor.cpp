529eb4d53d7abf7d03daa94e98c1059d0e3ebe9b
Jonathan S <beefywhale@gmail.com>
2019-01-10 17:30:10 -0600

    Create Actor.cpp

---
+++ Map/Actor.cpp
@@ -0,0 +1,18 @@
+#include "Actor.hpp"
+
+Actor::Actor(char* value, int x, int y, Color color) {
+    val = value;
+    position.x = x;
+    position.y = y;
+    colors = color;
+}
+
+void Actor::move(int x, int y) {
+    position.x = position.x + x;
+    position.y = position.y + y;
+}
+
+void Actor::setPosition(int x, int y) {
+    position.x = x;
+    position.y = y;
+}
