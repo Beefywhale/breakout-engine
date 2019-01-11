bda747d47e4e7209e17cb162d5ebfa9d4ab5b0e2
Jonathan <beefywhale@gmail.com>
2019-01-11 14:51:49 -0600

    deubgging stuff (lol)

--- Examples/main.cpp
+++ Examples/main.cpp
@@ -5,6 +5,7 @@
 // main function
 int main() {
 
+    std::cout << "test";
     // create an Engine object to process input and rendering.
     Engine engine = Engine("Title", 640, 480);
     std::cout << "test";
